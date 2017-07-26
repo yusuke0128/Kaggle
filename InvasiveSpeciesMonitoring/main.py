e841b5664457c399147d50004602c738d8200505
yusuke0128 <t.yuusuke0128@outlook.jp>
2017-07-26 12:01:16 +0900

    データ読み込み部分追加

--- InvasiveSpeciesMonitoring/main.py
+++ InvasiveSpeciesMonitoring/main.py
@@ -8,14 +8,35 @@ import time
 import matplotlib.pyplot as plt
 import pandas as pd
 import glob
+import re
+import cv2
+
+gpu_flag = 0
+
+if gpu_flag >= 0:
+    cuda.check_cuda_available()
+xp = cuda.cupy if gpu_flag >= 0 else np
+
+#辞書順から自然順に並び替えるメソッド
+def numericalSort(value):
+    numbers = re.compile(r'(\d+)')
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
+
 #データ読み込み
-trainData = glob.glob("./train/*")
-trainLabelDf = pd.read_csv("train_labels.csv")
-for i in trainData:
-	tmp = trainData
-	number_padded = "%03d"%2
-	print(number_padded)
-	trainData[i] = tmp.find("./train/"+number_padded+".jpg")
-print(trainData[0])
-
-
+trainDataSet = sorted(glob.glob("./dataset/train/*"),key=numericalSort)
+trainLabelDf = pd.read_csv("train_labels.csv") 
+trainR = 0
+trainRGBSet = [0]*len(trainDataSet)
+trainRSet = [0]*len(trainDataSet)
+for i in range(len(trainDataSet)):
+	trainDataSet[i] = cv2.imread(trainDataSet[i])
+	trainRGBSet[i] = cv2.split(trainDataSet[i])
+	trainR = trainRGBSet[i]
+	trainRSet[i] = trainR[1]
+cv2.imshow("orignal",trainRSet[1])
+cv2.waitKey()
+cv2.destroyAllWindows()
+trainData = trainData.astype(xp.float32)
+trainLabel = trainLabelDf["invasive"].value
