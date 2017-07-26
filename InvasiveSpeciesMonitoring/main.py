b35556cbcc109caba0e2b498748ec0977f83f2d8
yusuke0128 <t.yuusuke0128@outlook.jp>
2017-07-27 01:04:47 +0900

    色々変更

--- InvasiveSpeciesMonitoring/main.py
+++ InvasiveSpeciesMonitoring/main.py
@@ -18,10 +18,58 @@ if gpu_flag >= 0:
     cuda.check_cuda_available()
 xp = cuda.cupy if gpu_flag >= 0 else np
 
+batchsize = 100
+n_epoch = 20
 p  = pre.PreProc()
-#データ読み込み
+
+#学習データ読み込み
 trainDataURL = "./dataset/train/*"
 trainLabelDfURL = "train_labels.csv"
-print(trainDataURL)
 trainLabelSet = p.preLabel(trainLabelDfURL)
 trainRGBSet = p.dataToRGB(trainDataURL)
+#テストデータ読み込み
+testDataURL = "./dataset/test/*"
+testRGBSet = p.dataToRGB(testDataURL)
+#model定義
+
+model = chainer.FunctionSet(conv1=F.Convolution2D(3,30,20),
+                            conv2=F.Convolution2D(30,100,15),
+                            conv3=F.Convolution2D(100,20,50)
+                            l1=F.liner()
+                            l2=F.liner()
+                            l3= )
+
+if gpu_flag >= 0:
+	cuda.get_device(gpu_flag).use()
+	model.to_gpu()
+
+def forward():
+	x,t = chainer.Variable(trainRGBSet),chainer.Variable(trainLabelSet)
+	h = F.max_pooling_2d(F.relu(model.conv1(x)),10)
+	h = F.max_pooling_2d(F.relu(model.conv2(h)),10)
+	h = F.max_pooling_2d(F.relu(model.conv3(h)),10)
+	h = F.dropout(F.relu(model.l1(h)),train=train)
+	h = F.dropout(F.relu(model.l2(h)),train=train)
+	y = model.l3(h)
+	if train:
+		return F.softmax_cross_entropy(y,t)
+	else:
+		return F.accuracy(y,t)
+
+optimizer = optimizers.Adam()
+optimizer.setup(model)
+
+fp1 = open("accuracy.txt", "w")
+fp2 = open("loss.txt", "w")
+
+fp1.write("epoch\ttest_accuracy\n")
+fp2.write("epoch\ttrain_loss\n")
+
+
+#訓練ループ
+startTime = time.clock()
+for epoch in range(1,n_epoch+1)
+	print("epoch%d"%epoch)
+	perm = np.random.permutation(N)
+	sumLoss = 0
+
---
+++ InvasiveSpeciesMonitoring/preProc.py
@@ -0,0 +1,32 @@
+import glob
+import pandas as pd
+import re
+import sort
+import cv2
+import chainer
+import numpy as np
+from chainer import cuda
+
+class PreProc():
+	def __init__(self):
+		self.URL = ""
+		self.value=0
+
+	def dataToRGB(self,URL):
+		s = sort.Sort()
+		dataSet = sorted(glob.glob(URL),key=s.numericalSort)
+		RGBSet = [0]*len(dataSet)
+		GSet = [0]*len(dataSet)
+		BSet = [0]*len(dataSet)
+		for i in range(len(dataSet)):
+			dataSet[i] = cv2.imread(dataSet[i])
+			RGBSet[i] = cv2.split(dataSet[i])
+		RGBSet = np.array(RGBSet)
+		print(RGBSet.shape)
+		return RGBSet
+
+	def preLabel(self,URL):
+		labelDf = pd.read_csv(URL)
+		label = labelDf["invasive"].values
+		return label
+
